0d437f3d49910bedda73574468e56b1a23823328
lindw3 <willelind95@gmail.com>
2025-12-14 20:44:20 +0100

    Uppstart av app!

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "system-ui" }}>
+      <h1>Big Talk</h1>
+      <p>
+        En app för samtalsfrågor och reflektion.
+      </p>
+    </div>
+  );
+}
+
+export default App;
