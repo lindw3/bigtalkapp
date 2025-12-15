7dfe50a5ad05917db7e84db380f588551c215e10
lindw3 <willelind95@gmail.com>
2025-12-15 20:08:58 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/discussion-pwa/sw.js', { scope: '/discussion-pwa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/bigtalkapp/sw.js', { scope: '/bigtalkapp/' })})}
